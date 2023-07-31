f22a8a920cff68ff48b2106cfeb1094a358b2214
MANISH KUMAR <111644038+kmanisgithub@users.noreply.github.com>
2023-07-31 12:54:10 +0530

    Create Largest Element in an Array.cpp

---
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step 3.1 : Easy/Largest Element in an Array.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h> 
+int largestElement(vector<int> &arr, int n) {
+    // Write your code here.
+    int max=arr[0];
+    for(int i=0; i<arr.size(); i++)
+    if(arr[i]>max)
+    max=arr[i];
+    return max;
+}

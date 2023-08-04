2767cd9fcf3e543883ba4815af025652fdb4ee31
MANISH KUMAR <111644038+kmanisgithub@users.noreply.github.com>
2023-08-05 01:18:52 +0530

    Create Remove duplicates from Sorted array.cpp

---
+++ Step 3 : Solve Problems on Arrays [Easy -> Medium -> Hard]/Step 3.1 : Easy/Remove duplicates from Sorted array.cpp
@@ -0,0 +1,10 @@
+int removeDuplicates(vector<int> &arr, int n) {
+	// Write your code here.
+	int j=0;
+	for(int i=0; i<n; i++){
+		if(arr[i] !=arr[j]){
+			arr[++j]=arr[i];
+		}
+	}
+	return j+1;
+}

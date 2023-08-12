4723e7019a193d1d9a95ee5a5c2c7e598a3c83e1
MANISH KUMAR <111644038+kmanisgithub@users.noreply.github.com>
2023-08-12 23:23:46 +0530

    Create Capacity to Ship Packages within D  Days.cpp

---
+++ Step 4 : Binary Search [1D, 2D Arrays, Search Space]/Step 4.2 : BS on Answers/Capacity to Ship Packages within D  Days.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+int findDays(vector<int>&weights,int cap){
+    int days=1, load=0;
+    for(int i=0;i<weights.size(); i++){
+        if(weights[i]+load>cap){
+            days+=1;
+            load=weights[i];
+        }
+        else{
+            load+=weights[i];
+        }
+    }
+    return days;
+}
+int leastWeightCapacity(vector<int> &weights, int d)
+{
+    // Write your code here.
+    int low=*max_element(weights.begin(),weights.end());
+    int high=accumulate(weights.begin(),weights.end(),0);
+    while(low<=high){
+        int mid=(low+high)/2;
+        int numberOfDays=findDays(weights,mid);
+        if(numberOfDays<=d){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
+    return low;
+}
